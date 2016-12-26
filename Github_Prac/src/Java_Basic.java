8899b480413b7f63df6f9d089637768821d15699
Nin <ninumkma02@gmail.com>
2016-12-26 21:15:09 +0530

    Committed the java

---
+++ Github_Prac/src/Java_Basic.java
@@ -0,0 +1,23 @@
+
+public class Java_Basic
+{
+
+	public void method1()
+	{
+		System.out.println("This is MY Method_1 for drinking coffee");
+	}
+	
+	
+	
+	public void method2()
+	{
+		System.out.println("This is MY Method_2 for making Coffee");
+	}
+	
+	
+
+	public void method3()
+	{
+		System.out.println("This is My Method_3 for Buying Coffee ");
+	}
+}
